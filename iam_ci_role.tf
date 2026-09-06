557400ec32304df64250cf5e5955ebadc72d5902
l <17405573+LuisGi93@users.noreply.github.com>
2026-09-06 14:20:38 +0200

    feat(ci): add least-privilege OIDC CI role, S3 backend hardening and GitHub Actions workflow

--- iam_ci_role.tf
+++ iam_ci_role.tf
@@ -22,36 +22,98 @@ data "aws_iam_policy_document" "github_actions_trust" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:*"]
+      values   = ["repo:${var.github_repo}:pull_request"]
     }
   }
 }
 
-data "aws_iam_policy_document" "github_actions_ci_permissions" {
+data "aws_iam_policy_document" "github_actions_ci_plan_permissions" {
   statement {
-    sid       = "TerraformStateBucketList"
-    effect    = "Allow"
-    actions   = ["s3:ListBucket"]
+    sid    = "TerraformStateBucketRead"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetBucketAccelerateConfiguration",
+      "s3:GetBucketAcl",
+      "s3:GetBucketCors",
+      "s3:GetBucketEncryption",
+      "s3:GetBucketLifecycleConfiguration",
+      "s3:GetBucketLogging",
+      "s3:GetBucketPolicy",
+      "s3:GetBucketReplication",
+      "s3:GetBucketRequestPayment",
+      "s3:GetBucketTagging",
+      "s3:GetBucketVersioning",
+      "s3:GetBucketWebsite",
+      "s3:ListBucket",
+      "s3:GetBucketPublicAccessBlock"
+    ]
+
     resources = [aws_s3_bucket.s3_backend.arn]
   }
 
+
+
   statement {
-    sid       = "TerraformStateObjectAccess"
+    sid       = "TerraformStateLockfileWrite"
     effect    = "Allow"
-    actions   = ["s3:GetObject", "s3:PutObject"]
-    resources = ["${aws_s3_bucket.s3_backend.arn}/*"]
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.s3_backend.arn}/${var.state_key}.tflock"]
+  }
+
+
+  statement {
+    sid    = "TerraformStateObjectRead"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.s3_backend.arn}/${var.state_key}"
+    ]
   }
 
   statement {
-    sid       = "TerraformLockfileDelete"
+    sid    = "TerraformStateLockfileRead"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.s3_backend.arn}/${var.state_key}.tflock"
+    ]
+  }
+
+  statement {
+    sid       = "TerraformStateLockfileDelete"
     effect    = "Allow"
     actions   = ["s3:DeleteObject"]
-    resources = ["${aws_s3_bucket.s3_backend.arn}/*.tflock"]
+    resources = ["${aws_s3_bucket.s3_backend.arn}/${var.state_key}.tflock"]
   }
+
+
+  statement {
+    sid    = "IAMRoleRead"
+    effect = "Allow"
+    actions = [
+      "iam:GetRole",
+      "iam:GetRolePolicy",
+      "iam:ListAttachedRolePolicies",
+      "iam:ListRolePolicies"
+    ]
+    resources = [aws_iam_role.github_actions_ci.arn]
+  }
+
+
+
 }
 
 resource "aws_iam_role_policy" "github_actions_ci_permissions" {
   name   = "terraform-ci-permissions"
   role   = aws_iam_role.github_actions_ci.id
-  policy = data.aws_iam_policy_document.github_actions_ci_permissions.json
+  policy = data.aws_iam_policy_document.github_actions_ci_plan_permissions.json
 }
